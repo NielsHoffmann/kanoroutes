03f909a3db99257ea6df06f8da6cbaf45a2bded8
Niels <niels.hoffmann@hotmail.com>
2022-01-26 17:50:14 +0100

    nieuwe routes

--- routes.js
+++ routes.js
@@ -1,4 +1,7 @@
 var routeData = { "routes":
 ["/tracks/kagerplassen.gpx",
-  "/tracks/westzaan.gpx"
+  "/tracks/westzaan.gpx",
+  "/tracks/ijburg.gpx",
+  "/tracks/vinkeveense_plassen.gpx",
+  "/tracks/eilandspolder.gpx"
 ]}
