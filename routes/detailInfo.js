c4376658f3f536d47f2ea3c65296354614b4141b
Niels <niels.hoffmann@hotmail.com>
2022-02-08 16:56:41 +0100

    detail paginas toegevoegd

---
+++ routes/detailInfo.js
@@ -0,0 +1,9 @@
+//json array met detail informatie behorende bij de GPX files
+//format = naam van de gpx file (zonder .gpx extensie) : { naam, lengte, parkeren, instap, overige info}
+var routeInfo = {
+    "westzaan":'{ "naam":"Westzaan","lengte": "10km","parkeren":"langs de weg vlakbij de instap","instap":"mooie kanosteiger","overig":"ook een boothelling"}',
+"eilandspolder":'{ "naam":"Eilandspolder","lengte": "14,3km","parkeren":"De Rijp, Het Ouweland","instap":"lage slootkant met beschoeiing", "overig":"officiele kano aanlegplaats"}',
+"eilandspolder_10_km":'{ "naam":"Eilandspolder","lengte": "10km","parkeren":"De Rijp, Het Ouweland","instap":"lage slootkant met beschoeiing", "overig":"officiele kano aanlegplaats"}',
+"nieuwe_meer_schinkel_sloterplas":'{ "naam":"Nieuwe Meer, Schinkel Sloterplas","lengte": "16,9km","parkeren":"Akersluis","instap":"kanosteigers aan beide zijden van de sluis", "overig":"2x overdragen, Cramersluis en Nieuwemeersluis"}',
+"slotervaart_schinkel_nieuwe_meer":'{ "naam":"Slotervaart - Schinkel - Nieuwe meer","lengte": "16,9km","parkeren":"Akersluis","instap":"kanosteigers aan beide zijden van de sluis", "overig":"2x overdragen, Westlandgrachtsluis en Nieuwemeersluis"}'
+}
